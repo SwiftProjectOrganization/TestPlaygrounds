6a6711205193a83cc2fd032f0d8fa7167c9beb9f
Rob J Goedman <goedman@icloud.com>
2025-10-16 10:40:49 -0600

    Initial Commit

---
+++ Test playgrounds/Test_playgroundsApp.swift
@@ -0,0 +1,32 @@
+//
+//  Test_playgroundsApp.swift
+//  Test playgrounds
+//
+//  Created by Robert Goedman on 10/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Test_playgroundsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
